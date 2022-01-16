9091083fd99088da15267c72057114f8dd002b2f
affanalidawah <96285428+affanalidawah@users.noreply.github.com>
2022-01-16 12:35:34 -0600

    Cleaned useImportData and added comments

--- hooks/useImportData.js
+++ hooks/useImportData.js
@@ -1,25 +1,19 @@
 import { firebase } from "../firebase";
-import {
-  firestore,
-  getFirestore,
-  doc,
-  collection,
-  getDoc,
-} from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 
+// This is used to grab the Iqamah and Jumuah timings for the Masjid
 const useImportData = () => {
   const [iqamahTimings, setIqamahTimings] = useState("");
 
-  // database variable
+  // Create database object and grab relevant document
   const db = getFirestore();
-  const iqamahTimes = doc(db, "masjid/sanantonio/data/iqamah-times");
+  const iqamahTimesLocation = doc(db, "masjid/sanantonio/data/iqamah-times");
 
-  // mount
+  // Import data on mount
   useEffect(() => {
-    // initialize firebase data imports
     const getDocument = async () => {
-      const one = await getDoc(iqamahTimes);
+      const one = await getDoc(iqamahTimesLocation);
       setIqamahTimings(one.data());
     };
     getDocument();
