653978329b81311ef2ebae16fdfadb768edbbbef
affanalidawah <96285428+affanalidawah@users.noreply.github.com>
2022-01-16 12:32:08 -0600

    Cleaned up setUpAdhan and added comments

--- setUpAdhan.js
+++ setUpAdhan.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { View, Text } from "react-native";
 const adhan = require("adhan");
 
 const setUpAdhan = () => {
-  var adhan = require("adhan");
+  // Location of Masjid
   let latitude = 29.637107774550635;
   let longitude = -98.45155341131422;
+  var coordinates = new adhan.Coordinates(latitude, longitude);
+
+  // Dates for today and tomorrow
   var date = new Date();
   var nextDay = new Date();
   nextDay.setDate(nextDay.getDate() + 1);
+
+  // Parameters for Adhan timings
   var params = adhan.CalculationMethod.NorthAmerica();
-  var coordinates = new adhan.Coordinates(latitude, longitude);
+
+  // Initialize prayerTimes object that will be exported
   var prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+
+  // Grab value for the next day's Fajr time in new object
   const { fajr } = new adhan.PrayerTimes(coordinates, nextDay, params);
+
+  // Add next day's Fajr time to the original object and export it
   prayerTimes.fajrNext = fajr;
   return prayerTimes;
 };
